6ff740da187a9fe1c13f54f92a70fa1ae1294533
SeokyungKim <kskyung0624@gmail.com>
2022-09-14 11:01:04 -0700

    Fix : ts path build error

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '@exypress/(.*)': '<rootDir>/exypress/$1',
+    '@common/(.*)': '<rootDir>/common/$1',
+    '@src/(.*)': '<rootDir>/src/$1',
+  },
 };
