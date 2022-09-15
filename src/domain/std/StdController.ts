b0fe9c528374a7370665ea5935e9a615cb77679c
SeokyungKim <kskyung0624@gmail.com>
2022-09-15 06:25:44 -0700

    Fix : 검사 신청에 토큰 확인 추가

--- src/domain/std/StdController.ts
+++ src/domain/std/StdController.ts
@@ -1,12 +1,21 @@
+import { BaseHttpError } from '@common/error/HttpErrors';
 import statusCodes from '@common/messages/statusCodes';
 import logger from '@exypress/loaders/logger';
 import { Request, Response } from 'express';
+import AuthService from '../auth/AuthService';
 import StdService from './StdService';
 
 const stdService = new StdService();
+const authService = new AuthService();
 
 export default class StdController {
   public async stdRegister(req: Request, res: Response) {
+    const token = req.headers['authorization']?.split(' ')[1] as string;
+    await authService.verifyToken(token).catch((err) => {
+      logger.error(err);
+      return res.status(statusCodes.OK).json({ stats: 'nok', message: '로그인 시간이 만료되었습니다.' });
+    });
+
     const { doctor_id, address, address_code, store_address } = req.body;
     await stdService.createNewStd(doctor_id, address, address_code, store_address).catch((err) => {
       logger.error(err);
