630fbdf4b56b25235078a15930e4c66ecb32ff76
SeokyungKim <kskyung0624@gmail.com>
2022-09-15 03:15:46 -0700

    Feat : login validator 구현

--- src/domain/auth/AuthRouter.ts
+++ src/domain/auth/AuthRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import AuthController from './AuthController';
+import { loginValidator } from './AuthValidator';
 
 const router = Router();
 const authController = new AuthController();
 
-router.post('/login', authController.login);
+router.post('/login', loginValidator, authController.login);
 
 export default router;
